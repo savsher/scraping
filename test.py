265f9c6056e448767f16046ab25f5da591b43739
MrX <savsher@gmail.com>
2017-10-20 15:16:42 +0300

    begit to test

--- test.py
+++ test.py
@@ -1,8 +1,28 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
 
-url = "https://stock.inchcape.ru"
+pages = set()
+
+def getLinks(url):
+    global pages
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bsObj = BeautifulSoup(html.read())
+        data = bsObj.body.findAll("ul")
+    except AttributeError as e:
+        return None
+    return data
+
 
 if __name__ == '__main__':
+    url = "http://used-avtomir.ru/buy/new/"
     html = urlopen(url)
-    with open('test.html', 'w') as output_file:
-        output_file.write(html.encode('cp1251'))
+    bsObj = BeautifulSoup(html.read())
+    #data = bsObj.find_all("", {"class":"catalogue blocks"})
+    data = bsObj.find("ul", {"class":"catalogue blocks"})
+    for i in data.find_all("li"):
+        print(i.find("a").attrs["href"])
