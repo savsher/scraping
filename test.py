b523df0616766e5b1e3a18092d1edf29bdbcfb7d
MrX <savsher@gmail.com>
2017-10-24 16:44:34 +0300

    $(date)

--- test.py
+++ test.py
@@ -1,72 +1,52 @@
-from urllib.request import urlopen
-from urllib.error import HTTPError
-from urllib.parse import urlencode, quote_plus, unquote_plus
-
 from bs4 import BeautifulSoup
 
-pages = set()
+import requests
+
+s = requests.Session()
+s.headers.update({'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                               'Chrome/61.0.3163.79 Safari/537.36'})
 
 
+def load_pages()
 
 
-url = "http://vrn.used-avtomir.ru/buy/?"
-val = [
-    ('FILTER[MODEL]',''),
-    ('FILTER[BRAND]',''),
-    ('FILTER[PRICE][MIN]','0'),
-    ('FILTER[PRICE][MAX]','0'),
-    ('FILTER[YEAR][MIN]','1980'),
-    ('FILTER[YEAR][MAX]','2017'),
-    ('FILTER[MILEAGE][MIN]','0'),
-    ('FILTER[MILEAGE][MAX]','0'),
-    ('FILTER[TYPE][0]','new'),
-    ('FILTER[REGION]','406'),
-    ('FILTER[BODY]',''),
-    ('FILTER[ENGINE_TYPE]',''),
-    ('FILTER[TRANSMISSION]',''),
-    ('FILTER[DRIVE]','0'),
-    ('FILTER[COLOR]','0'),
-    ('FILTER[CABIN]','0'),
-    ('FILTER[DISKS]','0'),
-    ('FILTER[CLIMATE]','0'),
-    ('FILTER[AUDIO]','0'),
-    ('FILTER[SEAT_HEATING]','0'),
-    ('FILTER[PARKTRONICS]','0'),
-    ('FILTER[HEADLIGHTS]','0')
-]
+def getLinks(addition):
+    print(url)
+    html = requests.get(url+addition)
+    bsObj = BeautifulSoup(html.text)
+    data = bsObj.find("div", {"class": "catalogueContainer"})
+    x = data.find("ul", {"class": "catalogue blocks"})
+    for i in x.find_all("li"):
+        print(i.find("a").attrs["href"])
 
-fval = urlencode(val, doseq=True)
-print(fval)
-
-
-
-
-def getLinks(url):
-    global pages
-    try:
-        html = urlopen(url)
-    except HTTPError as e:
+    y = data.find("div", {"class": "pagination"})
+    if y is None:
         return None
-    try:
-        bsObj = BeautifulSoup(html.read())
-        data = bsObj.body.findAll("ul")
-    except AttributeError as e:
-        return None
-    return data
-
-
-"""
-http://vrn.used-avtomir.ru/buy/?FILTER[MODEL%5D=&FILTER[BRAND%5D=&FILTER[PRICE%5D[MIN%5D=0&FILTER[PRICE%5D[MAX%5D=0&FILTER[YEAR%5D[MIN%5D=1980&FILTER[YEAR%5D[MAX%5D=2017&FILTER[MILEAGE%5D[MIN%5D=0&FILTER[MILEAGE%5D[MAX%5D=300000&FILTER[TYPE%5D[0%5D=new&FILTER[REGION%5D=406&FILTER[BODY_TYPE%5D=&FILTER[ENGINE_TYPE%5D=&FILTER[TRANSMISSION%5D=&FILTER[DRIVE%5D=0&FILTER[COLOR%5D=0&FILTER[CABIN%5D=0&FILTER[DISKS%5D=0&FILTER[CLIMATE%5D=0&FILTER[AUDIO%5D=0&FILTER[SEAT_HEATING%5D=0&FILTER[PARKTRONICS%5D=0&FILTER[HEADLIGHTS%5D=0
-"""
-
+    else:
+        next_ref = y.find("a", {"id": "_next_page"})
+        if next_ref is None:
+            return None
+        else:
+            return (next_ref.attrs["href"])
 
 
 if __name__ == '__main__':
-    #url = "http://used-avtomir.ru/buy/new/"
-    print(url+fval)
-    html = urlopen(url+ fval)
-    bsObj = BeautifulSoup(html.read())
-    #data = bsObj.find_all("", {"class":"catalogue blocks"})
-    data = bsObj.find("ul", {"class":"catalogue blocks"})
-    for i in data.find_all("li"):
-        print(i.find("a").attrs["href"])
+
+    url = "http://vrn.used-avtomir.ru/buy/new/"
+    s = requests.Session()
+    s.headers.update({'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36'})
+    html = s.get(url)
+    bsObj = BeautifulSoup(html.text)
+    data = bsObj.find("div", {"class": "catalogueContainer"})
+    print(data)
+
+    payload = {'PAGEN_1':'2'}
+    html = s.get(url, params=payload)
+    bsObj = BeautifulSoup(html.text)
+    data = bsObj.find("div", {"class": "catalogueContainer"})
+    y = data.find("div", {"class": "pagination"})
+    next_ref = y.find("a", {"id": "_next_page"})
+    if next_ref is None:
+        print("asdfasdfadsf")
+    else:
+        print(next_ref)
