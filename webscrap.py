134b0e03d771514d60d5b248595325067416cea0
MrX <savsher@gmail.com>
2017-11-08 06:51:29 +0300

    Wed Nov  8 06:51:29 MSK 2017

--- webscrap.py
+++ webscrap.py
@@ -6,6 +6,14 @@ import sys
 import atexit
 import signal
 import time
+import requests
+from bs4 import BeautifulSoup
+import re
+import os
+import sqlite3
+import smtplib
+import email.utils
+from email.mime.text import MIMEText
 
 def daemonize(pidfile, *, stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
     if os.path.exists(pidfile):
@@ -51,9 +59,54 @@ def daemonize(pidfile, *, stdin='/dev/null', stdout='/dev/null', stderr='/dev/nu
 def main():
     sys.stdout.write('Daemon started with pid {}\n'.format(os.getpid()))
     while True:
-        sys.stdout.write('Test {}'.format(time.ctime()))
-        time.sleep(100)
+        sys.stdout.write('Test {}\n'.format(time.ctime()))
+        time.sleep(200)
 
+def web_scraping():
+    def get_links(s, page):
+        annex = "/buy/new/"
+        if bool(page):
+            html = s.get(url + annex, params=page)
+        else:
+            html = s.get(url + annex)
+        bsObj = BeautifulSoup(html.text)
+        data = bsObj.find("div", {"class": "catalogueContainer"})
+        if data is None:
+            return False
+        tmp = data.find("ul", {"class": "catalogue blocks"})
+        if tmp is None:
+            return False
+        for x in tmp.find_all("li"):
+            urlData.add((x.a.find('div', {'class': 'caption'}).text,
+                         x.a.find('div', {'class': 'price'}).text,
+                         x.a.find('div', {'class': 'price'}).next_sibling.strip(),
+                         x.a.attrs["href"]))
+
+        tmp = data.find("div", {"class": "pagination"})
+        if tmp is not None:
+            nextref = tmp.find("a", {"id": "_next_page"})
+            if nextref is not None:
+                x = re.split(r'=', re.split(r'\?', nextref.attrs["href"])[1])
+                get_links(s, {x[0]: x[1]})
+            return True
+        else:
+            return True
+
+    sys.stdout.write('Daemon <webscraping> started with pid {}\n'.format(os.getpid()))
+    while True:
+        url = "http://vrn.used-avtomir.ru"
+        urlData = set()
+        #baseData = set()
+        # Scrap data from site
+        with requests.Session() as s:
+            s.headers.update({'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)'
+                                            ' Chrome/61.0.3163.79 Safari/537.36'})
+            page = dict()
+            if get_links(s, page):
+                # urlData.pop()
+                sys.stdout.write('{} - get data from site'.format(time.clock()))
+        # Coffee break
+        time.sleep(20)
 
 if __name__ == '__main__':
     PIDFILE = '/tmp/webscrap.pid'
@@ -66,7 +119,7 @@ if __name__ == '__main__':
         except RuntimeError as e:
             print(e, file=sys.stderr)
             raise SystemExit(1)
-        main()
+        web_scraping()
     elif sys.argv[1] == 'stop':
         if os.path.exists(PIDFILE):
             with open(PIDFILE) as f:
